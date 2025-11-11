23ce95123966fd891b1a52ba4654782eb9b67c53
ZahraAlaali <zooz.alaali03@icloud.com>
2025-11-11 09:42:05 +0300

    setup

---
+++ Jo3aan/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Jo3aan project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Jo3aan.settings')
+
+application = get_asgi_application()
---
+++ Jo3aan/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Jo3aan project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Jo3aan.settings')
+
+application = get_wsgi_application()
