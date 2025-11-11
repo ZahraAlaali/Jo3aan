5c7b098c9c20a2eed3c153624891e9d236232331
ZahraAlaali <zooz.alaali03@icloud.com>
2025-11-11 15:50:32 +0300

    solve conflict

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,9 +7,12 @@ urlpatterns = [
     path("accounts/signup/", views.signup, name="signup"),
     #
     path("", views.home, name="home"),
-    path('restaurants/create/', views.RestaurantCreate.as_view(), name='restaurant_create'),
-    path('restaurants/',views.restaurants_index, name='restaurants_index')
-    ]
+    path(
+        "restaurants/create/",
+        views.RestaurantCreate.as_view(),
+        name="restaurant_create",
+    ),
+    path("restaurants/", views.restaurants_index, name="restaurants_index"),
     # profile
     path("profile/", views.profile, name="profile"),
     path("profile/create/", views.ProfileCreate.as_view(), name="craete_profile"),
