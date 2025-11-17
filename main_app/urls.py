24c387d557e979810dca5b48836762a4a754c79a
ZahraAlaali <zooz.alaali03@icloud.com>
2025-11-17 14:13:57 +0300

    reset password

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path, include
 from . import views
 
+# from views import ResetPasswordView
+# from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     # Sign Up
     path("accounts/signup/", views.signup, name="signup"),
     #
     path("", views.home, name="home"),
-
     # profile
     path("profile/", views.profile, name="profile"),
     path("profile/create/", views.ProfileCreate.as_view(), name="craete_profile"),
@@ -21,23 +22,50 @@ urlpatterns = [
         views.profile_user_update,
         name="profile_update",
     ),
-
-    #Restaurant
+    # path("password-reset/", ResetPasswordView.as_view(), name="password_reset"),
+    # path(
+    #     "password-reset-confirm/<uidb64>/<token>/",
+    #     auth_views.PasswordResetConfirmView.as_view(
+    #         template_name="users/password_reset_confirm.html"
+    #     ),
+    #     name="password_reset_confirm",
+    # ),
+    # path(
+    #     "password-reset-complete/",
+    #     auth_views.PasswordResetCompleteView.as_view(
+    #         template_name="users/password_reset_complete.html"
+    #     ),
+    #     name="password_reset_complete",
+    # ),
+    # Restaurant
     path("restaurants/", views.restaurants_index, name="restaurants_index"),
-    path('restaurants/<int:restaurant_id>/', views.restaurant_details, name='restaurant_details'),
-    path("restaurants/create/",views.RestaurantCreate.as_view(),
-    name="restaurant_create"),
-    path('restaurants/<int:pk>/update/', views.RestaurantUpdate.as_view(),name='restaurant_update'),
-    path('restaurants/<int:pk>/delete/', views.RestaurantDelete.as_view(),name='restaurant_delete'),
-
+    path(
+        "restaurants/<int:restaurant_id>/",
+        views.restaurant_details,
+        name="restaurant_details",
+    ),
+    path(
+        "restaurants/create/",
+        views.RestaurantCreate.as_view(),
+        name="restaurant_create",
+    ),
+    path(
+        "restaurants/<int:pk>/update/",
+        views.RestaurantUpdate.as_view(),
+        name="restaurant_update",
+    ),
+    path(
+        "restaurants/<int:pk>/delete/",
+        views.RestaurantDelete.as_view(),
+        name="restaurant_delete",
+    ),
     # item
     path("item/", views.ItemList.as_view(), name="item_list"),
     path("item/<int:pk>/", views.ItemDetail.as_view(), name="item_detail"),
     path("item/create/", views.ItemCreat.as_view(), name="item_create"),
     path("item/<int:pk>/update/", views.ItemUpdate.as_view(), name="item_update"),
     path("item/<int:pk>/delete/", views.ItemDelete.as_view(), name="item_delete"),
-    path('restaurants/<int:restaurant_id>/add_item', views.add_item, name='add_item'),
-
+    path("restaurants/<int:restaurant_id>/add_item", views.add_item, name="add_item"),
     # Cart
     path(
         "cart/add/<int:user_id>/<int:item_id>/<int:restaurant_id>/",
--- main_app/views.py
+++ main_app/views.py
@@ -18,6 +18,11 @@ from .forms import (
 import datetime
 from django import forms
 
+# reset password
+from django.urls import reverse_lazy
+from django.contrib.auth.views import PasswordResetView
+from django.contrib.messages.views import SuccessMessageMixin
+
 # Create your views here.
 
 
@@ -53,6 +58,19 @@ def signup(request):
             "error_message": error_message,
         },
     )
+
+
+# class ResetPasswordView(SuccessMessageMixin, PasswordResetView):
+#     template_name = "users/password_reset.html"
+#     email_template_name = "users/password_reset_email.html"
+#     subject_template_name = "users/password_reset_subject"
+#     success_message = (
+#         "We've emailed you instructions for setting your password, "
+#         "if an account exists with the email you entered. You should receive them shortly."
+#         " If you don't receive an email, "
+#         "please make sure you've entered the address you registered with, and check your spam folder."
+#     )
+#     success_url = reverse_lazy("home")
 
 
 # restaurant part
@@ -133,10 +151,15 @@ class RestaurantDelete(LoginRequiredMixin, DeleteView):
 def restaurant_details(request, restaurant_id):
     restaurant = Restaurant.objects.get(id=restaurant_id)
     item_form = ItemForm()
+    add_to_cart_form = AddToCartForm()
     return render(
         request,
         "restaurants/details.html",
-        {"restaurant": restaurant, "item_form": item_form},
+        {
+            "restaurant": restaurant,
+            "item_form": item_form,
+            "add_to_cart_form": add_to_cart_form,
+        },
     )
 
 
@@ -288,7 +311,7 @@ def viewCart(request, user_id):
         if restaurant.name not in restaurants:
             restaurants.append(restaurant.name)
         row.name = row.item.name
-        row.image = row.item.image
+        row.itemImage = row.item.itemImage
         row.total_price = row.item.price * row.quantity
         row.restaurant = restaurant.name
 
