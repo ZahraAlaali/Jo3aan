bc3d3012ad62dd8173b694dd2dd362e7bccf307a
HawraAlaam <hawraalaam@gmail.com>
2025-11-17 14:31:32 +0300

    checking branch

---
+++ main_app/forms.py
@@ -0,0 +1,38 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.forms import ModelForm
+from .models import User, Profile, CartDetails,Item
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ("username", "email")
+
+
+class CustomProfileCreationForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ["phone", "profileImage", "role"]
+
+
+class UpdateProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ["phone", "profileImage"]
+
+
+class UpdateUserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ["username"]
+
+class ItemForm(ModelForm):
+    class Meta:
+        model = Item
+        fields = ["name", "description","itemImage", "price"]
+
+class AddToCartForm(forms.ModelForm):
+    class Meta:
+        model = CartDetails
+        fields = ["quantity", "comment"]
